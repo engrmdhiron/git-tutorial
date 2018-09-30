cd3b0cf620dea1709c9517e1922a1c4b5b938f7a
proghiron <prog.mdhiron@gmail.com>
2018-10-01 01:55:24 +0600

    Second function add and call

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.printIn("Vs code is awesome...");
+
+        greeting("Sagor");
+    }
+
+
+
+    public static void main greeting(String name){
+        System.out.printIn("Welcome" . name);
+    }
+}
