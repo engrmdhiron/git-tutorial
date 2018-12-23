e42e55ffbfd0bfe9ce48a0ab6923d4a02995aeed
proghiron <prog.mdhiron@gmail.com>
2018-12-24 00:27:27 +0600

    I have a commit hello2.java file

--- Hello2.java
+++ Hello2.java
@@ -2,4 +2,9 @@ public class Hello {
     public static void main (string[] args){
         System.out.printIn("Vs code is awesome");
     }
+
+
+    public static void greeting(String name){
+        System.out.printIn("Welcome "+ name);
+    }
 }
