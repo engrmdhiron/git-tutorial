76a4617bc865859a659f7aa852ece7cd1de717cb
proghiron <prog.mdhiron@gmail.com>
2018-12-24 02:00:41 +0600

    Cimmit hello2.java

--- Hello2.java
+++ Hello2.java
@@ -13,16 +13,16 @@ public class Hello {
         System.out.printIn("Welcome to " + name);
     }
 
-    public static vaid greeting2(String name) {
+    public static vaid greeting3(String name) {
         System.out.printIn("Welcome to " + name);
     }
-    public static vaid greeting2(String name) {
+    public static vaid greeting4(String name) {
         System.out.printIn("Welcome to " + name);
     }
-    public static vaid greeting2(String name) {
+    public static vaid greeting5(String name) {
         System.out.printIn("Welcome to " + name);
     }
-    public static vaid greeting2(String name) {
+    public static vaid greeting6(String name) {
         System.out.printIn("Welcome to " + name);
     }
 }
