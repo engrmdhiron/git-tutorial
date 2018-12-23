0466ef77dbced05afea42135f822b48b2d7517d6
proghiron <prog.mdhiron@gmail.com>
2018-12-24 02:05:42 +0600

    commit

--- Hello2.java
+++ Hello2.java
@@ -7,7 +7,11 @@ public class Hello {
     public static void greeting(String name){
         System.out.printIn("Welcome "+ name);
 
-
+        message();
         greeting("Sagor");
+    }
+
+    public static void message(String[] body){
+        System.out.println("Message");
     }
 }
